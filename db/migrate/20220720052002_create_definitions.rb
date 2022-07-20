072c56c66ee71f0592530fda82dc8689f65d57ce
Rohit Nandy <rohitnandy39@gmail.com>
2022-07-20 11:48:01 +0530

    Extracted all the definitions of all the words and stored them in the definitions table

---
+++ db/migrate/20220720052002_create_definitions.rb
@@ -0,0 +1,10 @@
+class CreateDefinitions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :definitions do |t|
+      t.string :text
+      t.belongs_to :all_word, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_051303) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_052002) do
   create_table "all_words", force: :cascade do |t|
     t.string "word_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "definitions", force: :cascade do |t|
+    t.string "text"
+    t.integer "all_word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["all_word_id"], name: "index_definitions_on_all_word_id"
+  end
+
+  add_foreign_key "definitions", "all_words"
 end
